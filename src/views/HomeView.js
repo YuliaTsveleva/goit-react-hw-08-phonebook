98200a27b52c2d31902872b4bb64cf73b98a4994
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-02-03 22:09:29 +0200

    delete background image

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,22 +1,17 @@
-import { Typography, Stack } from '@mui/material';
+import { Typography } from '@mui/material';
 
 export default function HomeView() {
   return (
-    <div>
-      <Stack>
-        <Typography
-          variant="h2"
-          component="h1"
-          mt={3}
-          textAlign="center"
-          sx={{
-            color: 'rgb(9, 108, 255)',
-          }}
-        >
-          Welcome to your private phonebook!
-        </Typography>
-        {/* <img src={image} alt="homeImage" style={{ marginTop: '90px' }} /> */}
-      </Stack>
-    </div>
+    <Typography
+      variant="h2"
+      component="h1"
+      mt={3}
+      textAlign="center"
+      sx={{
+        color: 'rgb(9, 108, 255)',
+      }}
+    >
+      Welcome to your private phonebook!
+    </Typography>
   );
 }
