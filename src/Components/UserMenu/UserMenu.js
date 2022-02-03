474506955e5e081a4d2faf8570d9189f63b2d1fa
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-02-03 22:53:31 +0200

    refactoring userMenu styles

--- src/Components/UserMenu/UserMenu.js
+++ src/Components/UserMenu/UserMenu.js
@@ -1,35 +1,17 @@
-// import defaultAvatar from './default-avatar.png';
 import { useSelector, useDispatch } from 'react-redux';
 import { authSelectors, authOperations } from 'Redux/auth';
 import LetterAvatar from './Avatar';
-// перенести стили в модуль
-
-const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  avatar: {
-    marginRight: 4,
-  },
-  name: {
-    fontWeight: 700,
-    marginRight: 12,
-  },
-};
+import s from './UserMenu.module.css';
 
 export default function UserMenu() {
-  //   const avatar = defaultAvatar;
   const dispatch = useDispatch();
   const name = useSelector(authSelectors.getUserName);
   const isFetching = useSelector(authSelectors.getIsFetchingCurrentUser);
 
   return (
-    <div style={styles.container}>
-      {/* <img src={avatar} alt="" width="32" style={styles.avatar} /> */}
+    <div className={s.container}>
       <LetterAvatar name={isFetching ? 'Guest' : name} />
-
-      <span style={styles.name}>Hello, {isFetching ? 'Guest' : name}</span>
+      <span className={s.name}>Hello, {isFetching ? 'Guest' : name}</span>
       <button type="button" onClick={() => dispatch(authOperations.logOut())}>
         Log out
       </button>
