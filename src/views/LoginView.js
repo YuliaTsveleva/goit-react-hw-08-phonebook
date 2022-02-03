c848a12a3e5b401023be5589f1733536c1e10d60
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-02-03 15:50:15 +0200

    fixed loginForm component naming

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { authOperations } from '../Redux/auth';
-import AuthFormLogIn from '../Components/AuthForm';
+import AuthFormLogIn from '../Components/AuthLoginForm';
 
 export default function LoginView() {
   const dispatch = useDispatch();
