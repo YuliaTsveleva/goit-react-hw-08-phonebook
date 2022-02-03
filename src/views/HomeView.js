071747a787a50f7347d9dd0847f2fe66805f4053
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-02-03 21:49:35 +0200

    refactoring homeView, add background Image

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,17 +1,22 @@
-import Typography from '@mui/material/Typography';
+import { Typography, Stack } from '@mui/material';
 
 export default function HomeView() {
   return (
-    <Typography
-      variant="h2"
-      component="h1"
-      mt={3}
-      textAlign="center"
-      sx={{
-        color: 'rgb(9, 108, 255)',
-      }}
-    >
-      Welcome to your private phonebook!
-    </Typography>
+    <div>
+      <Stack>
+        <Typography
+          variant="h2"
+          component="h1"
+          mt={3}
+          textAlign="center"
+          sx={{
+            color: 'rgb(9, 108, 255)',
+          }}
+        >
+          Welcome to your private phonebook!
+        </Typography>
+        {/* <img src={image} alt="homeImage" style={{ marginTop: '90px' }} /> */}
+      </Stack>
+    </div>
   );
 }
