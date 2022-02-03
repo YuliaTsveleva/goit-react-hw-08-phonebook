d051dd0ce8dc93288ca3e9789846bb42b1bfb39e
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-02-03 15:47:04 +0200

    add color for avatar

--- src/Components/UserMenu/Avatar.js
+++ src/Components/UserMenu/Avatar.js
@@ -1,12 +1,30 @@
 import { Avatar } from '@mui/material';
 
 export default function LetterAvatar({ name }) {
-  console.log(name);
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let color = '#';
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.substr(-2);
+    }
+    return color;
+  }
+
   function stringAvatar(name) {
     return {
       children: `${name.split(' ')[0][0]}`,
     };
   }
 
-  return <Avatar {...stringAvatar(name)} />;
+  return (
+    <Avatar
+      {...stringAvatar(name)}
+      sx={{ marginRight: 1, bgcolor: stringToColor(name) }}
+    />
+  );
 }
