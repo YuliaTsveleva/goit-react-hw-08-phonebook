6c78e997e43609d87b4c7424b1dc3ef89f4e83c8
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-02-04 02:40:32 +0200

    add contacts errors handling

--- src/Redux/phonebook/contacts-operations.js
+++ src/Redux/phonebook/contacts-operations.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// поменять адрес бекенда
-// axios.defaults.baseURL = 'https://61f08b4e732d93001778eab3.mockapi.io';
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = createAsyncThunk(
@@ -12,7 +12,15 @@ export const fetchContacts = createAsyncThunk(
       const response = await axios.get('/contacts');
       return response.data;
     } catch (error) {
-      console.log(error.message);
+      if (error.response.status === 401) {
+        toast.error('Failed to get your contacts, contact technical support!');
+      } else if (error.response.status === 404) {
+        toast.error('The collection of such user does not exist!');
+      } else if (error.response.status === 500) {
+        toast.error('Failed to get your contacts, server error!');
+      } else {
+        toast.error('Failed to get your contacts, unknown error!');
+      }
       return rejectWithValue(error.message);
     }
   },
@@ -24,8 +32,14 @@ export const addContact = createAsyncThunk(
     try {
       const contact = { name, number, email };
       const response = await axios.post('/contacts', contact);
+      toast.success(`${response.data.name} added to your contacts!`);
       return response.data;
     } catch (error) {
+      if (error.response.status === 401) {
+        toast.error('Failed to add contact, contact technical support!');
+      } else {
+        toast.error('Failed to add contact, try again please!');
+      }
       return rejectWithValue(error.message);
     }
   },
@@ -36,8 +50,20 @@ export const deleteContact = createAsyncThunk(
   async (id, { rejectWithValue }) => {
     try {
       await axios.delete(`/contacts/${id}`);
+      toast.success('Contact deleted!');
       return id;
     } catch (error) {
+      if (error.response.status === 401) {
+        toast.error('Failed to delete contact, contact technical support!');
+      } else if (error.response.status === 404) {
+        toast.error(
+          'Failed to delete contact, the collection of such user does not exist!',
+        );
+      } else if (error.response.status === 500) {
+        toast.error('Failed to delete contact, server error!');
+      } else {
+        toast.error('Failed to delete contact, unknown error!');
+      }
       return rejectWithValue(error.message);
     }
   },
